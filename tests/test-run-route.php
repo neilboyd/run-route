8fe4929d4ad0f9a62ba2b379b663ae8c5f07a378
Neil Boyd <NeilBoyd@gmail.com>
2014-11-13 07:18:22 +0100

    Write some tests

--- tests/test-run-route.php
+++ tests/test-run-route.php
@@ -6,14 +6,10 @@ class RunRouteTest extends WP_UnitTestCase {
 
     // test that the shortcode is converted to the basic empty tag when written to a post
     public function test_create_and_get_empty_tag() {
-        // http://codesymphony.co/writing-wordpress-plugin-unit-tests/
 
-        $expected = <<<EOF
-<div class='run-route'></div>
-EOF;
+        $expected = "<div class='run-route'></div>";
 
-        // $post will be an instance of WP_Post
-        $post = $this->factory->post->create_and_get(array('post_content' => '[run_route page=1]'));
+        $post = $this->factory->post->create_and_get(array('post_content' => '[run_route]'));
         $actual = apply_filters( 'the_content', $post->post_content );
 
         $this->assertEquals($expected, trim( $actual ));
@@ -21,15 +17,79 @@ EOF;
 
     // test that the shortcode is converted to the basic empty tag when calling apply_filters
     public function test_empty_tag() {
-        // http://codesymphony.co/writing-wordpress-plugin-unit-tests/
 
-        $expected = <<<EOF
-<div class='run-route'></div>
-EOF;
+        $expected = "<div class='run-route'></div>";
 
-        // $post will be an instance of WP_Post
-        $actual = apply_filters( 'the_content', '[run_route page=1]' );
+        $actual = apply_filters( 'the_content', '[run_route]' );
 
         $this->assertEquals($expected, trim( $actual ));
     }
+
+    public function test_endomondo_without_quotes() {
+
+        $actual = apply_filters( 'the_content', '[run_route endomondo=123]' );
+
+        $this->assertContains("<div class='run-route'>", $actual);
+        $this->assertContains("<span class='endomondo'>", $actual);
+        $this->assertContains("<a href='http://endomondo.com/routes/123'>", $actual);
+        $this->assertContains("<img class='run-route-image'", $actual);
+    }
+
+    public function test_endomondo() {
+
+        $actual = apply_filters( 'the_content', "[run_route endomondo='123']" );
+
+        $this->assertContains("<div class='run-route'>", $actual);
+        $this->assertContains("<span class='endomondo'>", $actual);
+        $this->assertContains("<a href='http://endomondo.com/routes/123'>", $actual);
+        $this->assertContains("<img class='run-route-image'", $actual);
+        $this->assertNotContains("runkeeper", $actual);
+    }
+
+    // test that when runkeeper only user then it just shows empty tag
+    public function test_rk_only_user() {
+
+        $expected = "<div class='run-route'></div>";
+
+        $actual = apply_filters( 'the_content', "[run_route rk_user='123']" );
+
+        $this->assertEquals($expected, trim( $actual ));
+    }
+
+    // test that when runkeeper only route then it just shows empty tag
+    public function test_rk_only_route() {
+
+        $expected = "<div class='run-route'></div>";
+
+        $actual = apply_filters( 'the_content', "[run_route rk_route='789']" );
+
+        $this->assertEquals($expected, trim( $actual ));
+    }
+
+    public function test_runkeeper() {
+
+        $actual = apply_filters( 'the_content', "[run_route rk_user='123' rk_route='789']" );
+
+        $this->assertContains("<div class='run-route'>", $actual);
+        $this->assertContains("<span class='runkeeper'>", $actual);
+        $this->assertContains("<a href='http://runkeeper.com/user/123/route/789'>", $actual);
+        $this->assertContains("<img class='run-route-image'", $actual);
+        $this->assertNotContains("endomondo", $actual);
+    }
+
+    public function test_endomondo_and_runkeeper() {
+
+        $actual = apply_filters( 'the_content', "[run_route endomondo='123' rk_user='123' rk_route='789']" );
+
+        $this->assertContains("<div class='run-route'>", $actual);
+        $this->assertContains("<span class='endomondo'>", $actual);
+        $this->assertContains("<a href='http://endomondo.com/routes/123'>", $actual);
+        $this->assertContains("<img class='run-route-image'", $actual);
+
+        $this->assertContains("<div class='run-route'>", $actual);
+        $this->assertContains("<span class='runkeeper'>", $actual);
+        $this->assertContains("<a href='http://runkeeper.com/user/123/route/789'>", $actual);
+        $this->assertContains("<img class='run-route-image'", $actual);
+    }
+
 }
